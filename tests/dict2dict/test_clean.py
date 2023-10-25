2435bbd952618a53190dc2fcd605dcd563859288
lykmapipo <lallyelias87@gmail.com>
2023-10-25 21:34:53 +0300

    test(clean): improve falsey scenarios

--- tests/dict2dict/test_clean.py
+++ tests/dict2dict/test_clean.py
@@ -9,18 +9,26 @@ from dict2dict import omit_falsey
 @pytest.mark.parametrize(
     "source",
     [
-        (None, {}),
-        ({}, {}),
-        ({"a": 1, "b": None}, {"a": 1}),
-        ({"a": 1, "b": False}, {"a": 1}),
-        ({"a": 1, "b": 0}, {"a": 1}),
-        ({"a": 1, "b": []}, {"a": 1}),
-        ({"a": 1, "b": {}}, {"a": 1}),
-        ({"a": 1, "b": ()}, {"a": 1}),
+        (None, {}),  # handled
+        ({}, {}),  # handled
+        ({"a": 1, "b": None}, {"a": 1}),  # handled
+        ({"a": 1, "b": False}, {"a": 1}),  # handled
+        ({"a": 1, "b": 0}, {"a": 1}),  # handled
+        ({"a": 1, "b": 0.0}, {"a": 1}),  # handled
+        ({"a": 1, "b": 0j}, {"a": 1}),  # handled
+        ({"a": 1, "b": ""}, {"a": 1}),  # handled
+        ({"a": 1, "b": " "}, {"a": 1, "b": " "}),  # ignored
+        ({"a": 1, "b": []}, {"a": 1}),  # handled
+        ({"a": 1, "b": [None]}, {"a": 1, "b": [None]}),  # ignored
+        ({"a": 1, "b": {}}, {"a": 1}),  # handled
+        ({"a": 1, "b": {"c": None}}, {"a": 1, "b": {"c": None}}),  # ignored
+        ({"a": 1, "b": ()}, {"a": 1}),  # handled
+        ({"a": 1, "b": ("c", None)}, {"a": 1, "b": ("c", None)}),  # ignored
         (
-            {"a": 1, "b": "b", "c": 2.0, "d": True},
-            {"a": 1, "b": "b", "c": 2.0, "d": True},
-        ),
+            {"a": 1, "b": "b", "c": 2.0, "d": True, "e": 1j},
+            {"a": 1, "b": "b", "c": 2.0, "d": True, "e": 1j},
+        ),  # handled
+        # TODO: more falsey
     ],
 )
 def test_omit_falsey(source: Tuple[Dict[str, Any], Dict[str, Any]]) -> None:
